4a03d7a385df9eb55ea9ed42cb5105318a97a997
truland <tnfirebird1979@gmail.com>
2020-06-14 14:10:49 -0500

    Added in lots of virtual functions, Overloaded the Solve for the base class. Lots of outlines of functions missing their guts just to test compiling

--- GeneticToolkit/GASolver/include/GASolver.hpp
+++ GeneticToolkit/GASolver/include/GASolver.hpp
@@ -3,22 +3,45 @@
 
 #include <vector>
 #include <utility>
+#include <chrono>
+#include <iostream>
+#include <string>
+#include <tuple>
 
 #include "Gene.hpp"
 #include "MersenneTwister.hpp"
 
 class GASolver{
-	private:
-		MersenneTwister RandomGenerator;
+	protected:
+		MersenneTwister* RandomGenerator;
 		std::vector<Gene> Population;
 		std::vector<std::pair<double,double>> Bounds;
 		std::vector<double> FitVals;
+
+		std::vector<std::pair<double,double>> OneDimData;
+		std::vector<std::tuple<double,double,double>> TwoDimData;
 		
-		int NumVars;
 		int PopSize;		
 		double MutateRate;
+		double Tolerance;
+		int Generations;
+
+		double Chi2(const std::vector<double>&,const std::vector<double>&);
+		virtual void Mutate();
+		void Rank();
+		int Partition(std::vector<Gene>&,std::vector<double>&,const int&,const int&);
+		void QuickSortRec(std::vector<Gene>&,std::vector<double>&,const int&,const int&);
 	public:
-		GASolver(const int&,const int&,const std::vector<std::pair<double,double>>&,const double&);
+		GASolver(const int&,const int&,const std::vector<std::pair<double,double>>&,const double&,const int&,const double&);
+		~GASolver();
+
+		void LoadOneDimData(const std::vector<double>&,const std::vector<double>&);
+		void LoadTwoDimData(const std::vector<double>&,const std::vector<double>&,const std::vector<double>&);
+		void Print(std::string opt = "");
+
+		virtual std::tuple<Gene,double,int> Solve();
+		virtual void EvaluateGeneration();
+		virtual void Function();
 };
 
 #endif
--- GeneticToolkit/GASolver/src/GASolver.cpp
+++ GeneticToolkit/GASolver/src/GASolver.cpp
@@ -1,20 +1,115 @@
 #include "GASolver.hpp"
 
-GASolver::GASolver(const int& numvars,const int& popsize,const std::vector<std::pair<double,double>>& bounds,const double& mutate){
+GASolver::GASolver(const int& numvars,const int& popsize,const std::vector<std::pair<double,double>>& bounds,const double& mutate,const int& numgens,const double& tol){
 	if( numvars <= 0 )
 		throw "GASolver needs more than one variable to minimize";
 
-	this->Population = std::vector<Gene>(numvars,Gene(popsize));
-	this->FitVals.reserve(popsize);
+	this->Population = std::vector<Gene>(popsize,Gene(numvars));
+	this->FitVals = std::vector<double>(popsize,0.0);
 	this->MutateRate = mutate;
 	this->Bounds = bounds;
+	this->PopSize = popsize;
+	this->Generations = numgens;
+	this->Tolerance = tol;
+
+	this->RandomGenerator = new MersenneTwister();
+	this->RandomGenerator->init_genrand(std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()).count());
 
 	double upper = 0.0;
 	double lower = 0.0;	
-	for( int ii = 0; ii < numvars; ++ii ){
-		upper = bounds.at(ii).second;
-		lower = bounds.at(ii).first;
-		for( int jj = 0; jj < popsize; ++jj )
-			this->Population.at(ii).PushBack((upper - lower)*this->RandomGenerator.random() + lower);
+	
+	for( unsigned int jj = 0; jj < numvars; ++jj ){
+		upper = bounds.at(jj).second;
+		lower = bounds.at(jj).first;
+		for( unsigned int ii = 0; ii < popsize; ++ii )
+			this->Population.at(ii).EmplaceBack((upper - lower)*this->RandomGenerator->random() + lower);	
 	}
+}
+
+GASolver::~GASolver(){
+	delete this->RandomGenerator;
+	this->RandomGenerator = nullptr;
+}
+
+void GASolver::Print(std::string opt){
+	if( opt != "" )
+		this->RandomGenerator->print();
+
+	for( unsigned int ii = 0; ii < this->PopSize; ++ii )
+		std::cout << this->Population.at(ii) << '\n';
+}
+
+void GASolver::LoadOneDimData(const std::vector<double>& x,const std::vector<double>& y){
+	if( x.size() != y.size() )
+		throw "size of x not equal to size of y";
+	
+	this->OneDimData.reserve(x.size());
+	for( unsigned int ii = 0; ii < x.size(); ++ii )
+		OneDimData.emplace_back(std::make_pair(x.at(ii),y.at(ii)));
+}
+
+void GASolver::LoadTwoDimData(const std::vector<double>& x,const std::vector<double>& y,const std::vector<double>& z){
+	if( x.size() != y.size() )
+		throw "size of x not equal to size of y";
+	if( x.size() != z.size() )
+		throw "size of x not equal to size of z";
+
+	this->TwoDimData.reserve(x.size());
+	for( unsigned int ii = 0; ii < x.size(); ++ii )
+		TwoDimData.emplace_back(std::make_tuple(x.at(ii),y.at(ii),z.at(ii)));
+}
+		
+double GASolver::Chi2(const std::vector<double>& measured,const std::vector<double>& model){
+	//write chi2 calculation
+}
+
+void GASolver::Rank(){
+	//write quicksort for ranking the genes base on FitVals
+	this->QuickSortRec(this->Population,this->FitVals,0,this->PopSize);
+}
+
+void GASolver::Mutate(){
+}
+		
+void GASolver::EvaluateGeneration(){
+}
+
+void GASolver::Function(){
+}
+		
+int GASolver::Partition(std::vector<Gene>& population,std::vector<double>& fitness,const int& first,const int& last){
+	int ii = first;
+	int jj = last + 1;
+	while( ii <= jj ){
+		do ++ii;
+		while( fitness[ii] < fitness[first] );
+		do --jj;
+		while( fitness[first] < fitness[jj] );
+		if( ii < jj ){
+			std::swap(fitness[ii],fitness[jj]);
+			std::swap(population[ii],population[jj]);
+		}
+	}
+	std::swap(fitness[ii],fitness[jj]);
+	std::swap(population[ii],population[jj]);
+	return jj;
+}
+
+void GASolver::QuickSortRec(std::vector<Gene>& population,std::vector<double>& fitness,const int& first,const int& last){
+	if( first < last ){
+		int pivot = this->Partition(population,fitness,first,last);
+		this->QuickSortRec(population,fitness,first,pivot - 1);
+		this->QuickSortRec(population,fitness,pivot,last);
+	}
+}
+
+std::tuple<Gene,double,int> GASolver::Solve(){
+	int currgen = 0;
+	
+	do{
+		EvaluateGeneration();
+		++currgen;
+	}while( currgen <= this->Generations and FitVals.at(0) >= this->Tolerance );
+	
+	return std::make_tuple(this->Population.at(0),this->FitVals.at(0),currgen);
 }
